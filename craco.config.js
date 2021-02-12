67d71286539e8caa48e38336c840332c39dca232
tibizy <tobisuleodu@ymail.com>
2021-02-12 19:28:56 +0100

    commit initial files

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+/* craco.config.js */
+const CracoLessPlugin = require('craco-less');
+
+
+module.exports = {
+    plugins: [
+        {
+          plugin: CracoLessPlugin,
+          options: {
+            lessLoaderOptions: {
+              lessOptions: {
+                modifyVars: { 
+                    '@primary-color': '#FA4A84',
+                    '@btn-height-base': '50px',
+                    '@input-height-base': '44px',
+                    '@input-bg': '#F4F8FB',
+                    '@@input-hover-border-color': ''
+                 },
+                javascriptEnabled: true,
+              },
+            },
+          },
+        },
+    ],
+};
