c76e980719eb02340187763fef1a4164835e2fad
tibizy <tobisuleodu@ymail.com>
2021-02-13 06:42:25 +0100

    temporarily remove procfile and server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
